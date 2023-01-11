3f59ed143ec2bad04dd112f79b09085e5df7d66a
ajeet-digi <ajha@digisprint.com>
2023-01-11 12:12:32 +0530

    formdata with single useState and useRef

--- src/App.js
+++ src/App.js
@@ -1,78 +1,98 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import './App.css';
 
 function App() {
 
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [dob, setDob] = useState("");
-  const [image, setImage] = useState("");
 
-  const [formData, setFormData] = useState({});
   const [isSubmitted, setIsSubmitted] = useState(false)
 
-  
+  const [formData, setFormData] = useState({
+    name:"",
+    email:"",
+    dob:"",
+    image:""
+  });
 
 
-  const handleName = e => {
-    setName(e.target.value);
-  }
+  const [submittedFormData, setSubmittedFormData] = useState({
+    name:"",
+    email:"",
+    dob:"",
+    image:""
+  });
 
-  const handleEmail = e => {
-    setEmail(e.target.value);
-  }
-
-  const handleDob = e => {
-    setDob(e.target.value);
-  }
-
-  const handleFile = e => {
-    setImage(URL.createObjectURL(e.target.files[0]));
-  }
+  const imageRef = useRef(null);
 
   const handleForm = e => {
-    e.preventDefault();
     setFormData({
-      name,
-      email,
-      dob,
-      image
-    });
+      ...formData,
+      [e.target.name] : e.target.files && e.target.files[0] ? URL.createObjectURL(e.target.files[0]) : e.target.value
+    })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setSubmittedFormData(formData);
     setIsSubmitted(true);
+    setFormData({
+      name:"",
+      email:"",
+      dob:"",
+      image:""
+    })
+
+    imageRef.current.value=null
+  }
+
+  const handleDelete = () => {
+    setFormData({
+      name:"",
+      email:"",
+      dob:"",
+      image:""
+    })
+
+    imageRef.current.value=null
+    setIsSubmitted(false);
+  }
+
+  const handleEdit = () => {
+    setFormData(submittedFormData)
   }
 
   return (
     <div className="App">
-      <form onSubmit={handleForm}>
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor='name'>Name</label>
-          <input value={name} type="text" onChange={handleName}/>
+          <input name='name' value={formData.name} type="text"  onChange={handleForm}/>
         </div>
         <div>
           <label htmlFor='email'>Email</label>
-          <input value={email} type="email" onChange={handleEmail} />
+          <input name="email" value={formData.email} type="email"  onChange={handleForm} />
         </div>
         <div>
           <label htmlFor='dob'>D-O-B</label>
-          <input value={dob} type="date" onChange={handleDob}/>
+          <input name="dob" value={formData.dob} type="date"  onChange={handleForm}/>
         </div>
         <div>
           <label htmlFor='image'>Image</label>
-          <input type="file" onChange={handleFile}/>
+          <input name="image" type="file" ref={imageRef} onChange={handleForm}/>
         </div>
         <div>
-          <input type="submit" value="Save" />
+          <input type="submit" value="Save" onClick={handleSubmit}/>
         </div>
       </form>
 
       {
        isSubmitted && (
         <div className='user-info'>
-            <img src={image} alt={name} />
-            <p>{name}</p>
-            <p>{email}</p>
-            <p>{dob}</p>
-
+            <img src={submittedFormData.image} alt={submittedFormData.name} />
+            <p>{submittedFormData.name}</p>
+            <p>{submittedFormData.email}</p>
+            <p>{submittedFormData.dob}</p>
+            <button onClick={handleEdit}>Edit</button>
+            <button onClick={handleDelete}>Delete</button>
         </div>
        )
       }
